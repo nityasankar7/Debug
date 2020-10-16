194b18ff366f374f326dd5a198b01506d40ce2a2
nityasankar7 <70414577+nityasankar7@users.noreply.github.com>
2020-10-16 18:01:24 -0400

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,34 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    var options = {
+      density: 2.0,
+      restitution:0.2
+  }
+    super(x,y,50,50,options);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory =[];
+    this.Visiblity = 255;
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position); //[[x1,y1],[x2,y2],[x3,y3]]
+    }
+   
+
+    for(var i=0; i<this.trajectory.length; i++){
+      push();
+      this.Visiblity = this.Visiblity -0.5;
+      tint(255,this.Visiblity);
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+      pop ();
+    }
+  }
+}
